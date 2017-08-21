eebff08fa2a428e9be9a905b47f4e9daec3215c0
Joe <lsz2006.ad@gmail.com>
2017-08-21 18:03:15 +0100

    edit funddata.py

--- funddata.py
+++ funddata.py
@@ -14,19 +14,19 @@ import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 
 # 开始日期
-startdate='2012-08-01'
+startdate = '2012-08-01'
 # 结束日期
-enddate='2017-08-31'
+enddate = '2017-08-31'
 # 代码列表
-codelist=['160213', '160716', '110007']
+codelist = ['160213', '160716', '110007']
 # 组合权重
-weightlist=[1/3, 1/3, 1/3]
+weightlist = [1/3, 1/3, 1/3]
 
 urls=['http://data.funds.hexun.com/outxml/detail/openfundnetvalue.aspx?fundcode='
       +fundcode+'&startdate='+startdate+'&enddate='+enddate
       for fundcode in codelist]
-namelist=[]
-timeserieslist=[]
+namelist = []
+timeserieslist = []
 
 for url in urls:
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -40,25 +40,26 @@ for url in urls:
             datelist.append(datetime.strptime(child.find('fld_enddate').text, '%Y-%m-%d'))
             valuelist.append(float(child.find('fld_netvalue').text))
     
-    valuelist=[i/valuelist[-1] for i in reversed(valuelist)]
-    datelist=list(reversed(datelist))
-    codename=tree.find('fundcode').text + ' ' + tree.find('fundname').text
+    valuelist = [i/valuelist[-1] for i in reversed(valuelist)]
+    datelist = list(reversed(datelist))
+    codename = tree.find('fundcode').text + ' ' + tree.find('fundname').text
     namelist.append(codename)
-    print(codename, len(datelist), len(valuelist))
+    print(codename, "{:.2f}".format(valuelist[-1]))
     x = np.array(datelist)
     y = np.array(valuelist)
     ts = pd.Series(y, index=x, name=codename)
     timeserieslist.append(ts)
-    plt.plot(x,y)
+    plt.plot(x, y)
 
-df=pd.concat(timeserieslist, axis=1)
+df = pd.concat(timeserieslist, axis=1)
 for i in range(len(weightlist)):
     df[namelist[i]] = df[namelist[i]] * weightlist[i]
 
-portfolioname='portfolio'
+portfolioname = 'portfolio'
 df[portfolioname] = df.sum(axis=1, skipna=False)
 plt.plot(df[portfolioname])
 myfont = fm.FontProperties(fname='C:/Windows/Fonts/msyh.ttc')
 namelist.append(portfolioname+' '+', '.join(["{:.2f}".format(i) for i in weightlist]))
 plt.legend(namelist, prop=myfont)
+plt.grid()
 plt.show()
