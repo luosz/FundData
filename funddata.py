07086668239e5adf0c11af198c8b2a90a740ee6c
Joe <lsz2006.ad@gmail.com>
2017-08-22 17:35:49 +0100

    add support for command line arguments

--- funddata.py
+++ funddata.py
@@ -12,54 +12,70 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
+import sys
 
-# 开始日期
-startdate = '2012-08-01'
-# 结束日期
-enddate = '2017-08-31'
-# 代码列表
-codelist = ['160213', '160716', '110007']
-# 组合权重
-weightlist = [1/3, 1/3, 1/3]
+if __name__ == "__main__":
 
-urls=['http://data.funds.hexun.com/outxml/detail/openfundnetvalue.aspx?fundcode='
-      +fundcode+'&startdate='+startdate+'&enddate='+enddate
-      for fundcode in codelist]
-namelist = []
-timeserieslist = []
-
-for url in urls:
-    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    data = urlopen(req).read()
-    tree = ET.fromstring(data)
-    datelist = []
-    valuelist = []
+    startdate = '2013-01-01'
+    enddate = '2017-08-18'
+    codelist = ['160706', '050025', '000071']
     
-    for child in tree:
-        if child.tag == 'Data':
-            datelist.append(datetime.strptime(child.find('fld_enddate').text, '%Y-%m-%d'))
-            valuelist.append(float(child.find('fld_netvalue').text))
+    # read command line arguments
+    if len(sys.argv) > 1:
+        startdate = sys.argv[1]   
+    if len(sys.argv) > 2:
+        enddate = sys.argv[2]
+    if len(sys.argv) > 3:
+        del codelist[:]
+        codelist = sys.argv[3:]
+
+    # portfolio weights
+    weightlist = [1/len(codelist) for i in codelist]
     
-    valuelist = [i/valuelist[-1] for i in reversed(valuelist)]
-    datelist = list(reversed(datelist))
-    codename = tree.find('fundcode').text + ' ' + tree.find('fundname').text
-    namelist.append(codename)
-    print(codename, "{:.2f}".format(valuelist[-1]))
-    x = np.array(datelist)
-    y = np.array(valuelist)
-    ts = pd.Series(y, index=x, name=codename)
-    timeserieslist.append(ts)
-    plt.plot(x, y)
-
-df = pd.concat(timeserieslist, axis=1)
-for i in range(len(weightlist)):
-    df[namelist[i]] = df[namelist[i]] * weightlist[i]
-
-portfolioname = 'portfolio'
-df[portfolioname] = df.sum(axis=1, skipna=False)
-plt.plot(df[portfolioname])
-myfont = fm.FontProperties(fname='C:/Windows/Fonts/msyh.ttc')
-namelist.append(portfolioname+' '+', '.join(["{:.2f}".format(i) for i in weightlist]))
-plt.legend(namelist, prop=myfont)
-plt.grid()
-plt.show()
+    print(startdate, enddate, codelist)
+    
+    urls = ['http://data.funds.hexun.com/outxml/detail/openfundnetvalue.aspx?fundcode='
+          +fundcode+'&startdate='+startdate+'&enddate='+enddate
+          for fundcode in codelist]
+    namelist = []
+    timeserieslist = []
+    
+    for url in urls:
+        req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+        data = urlopen(req).read()
+        tree = ET.fromstring(data)
+        datelist = []
+        valuelist = []
+        
+        for child in tree:
+            if child.tag == 'Data':
+                datelist.append(datetime.strptime(child.find('fld_enddate').text, '%Y-%m-%d'))
+                valuelist.append(float(child.find('fld_netvalue').text))
+        
+        valuelist = [i/valuelist[-1] for i in reversed(valuelist)]
+        datelist = list(reversed(datelist))
+        fundname = tree.find('fundname').text
+        namelist.append(fundname)
+        x = np.array(datelist)
+        y = np.array(valuelist)
+        ts = pd.Series(y, index=x, name=fundname)
+        timeserieslist.append(ts)
+    
+    df = pd.concat(timeserieslist, axis=1)
+    df2 = df.copy()
+    for i in range(len(weightlist)):
+        df2[namelist[i]] = df2[namelist[i]] * weightlist[i]
+    
+    portfolioname = 'portfolio'
+    df[portfolioname] = df2.sum(axis=1, skipna=False)
+    std = df.std()
+    retstd = (df.iloc[[-1]] - 1) / std
+    datatable = df.iloc[[0,-1]].append(std.rename('standard deviation')).append(retstd.iloc[0].rename('return/standard deviation'))
+    print(datatable)
+    
+    plt.plot(df)
+    myfont = fm.FontProperties(fname='C:/Windows/Fonts/msyh.ttc')
+    namelist.append(portfolioname+' '+','.join(["{:.2f}".format(i) for i in weightlist]))
+    plt.legend(namelist, prop=myfont)
+    plt.grid()
+    plt.show()
