13f01b06f8efe7e0fbbd248558b34c6e47f34334
Joe <lsz2006.ad@gmail.com>
2017-08-24 11:24:22 +0100

    comment the code

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ The code was written in and tested with Python 3.6 (Anaconda 4.4.0).
 ## Usage
 #### Run funddata.py with command line arguments of start date, end date and a list of fund codes
 #### Example:
-python funddata.py 2013-01-01 2017-08-31 160706 165511 110011 040008
+python funddata.py 2014-01-01 2017-08-31 160706 165511 040008 110022 110011 020003 160211
 
 ![Screenshot](screenshot.png)
--- funddata.py
+++ funddata.py
@@ -16,11 +16,11 @@ import sys
 
 if __name__ == "__main__":
 
-    startdate = '2013-01-01'
+    startdate = '2014-01-01'
     enddate = '2017-08-31'
-    codelist = ['160706', '165511', '110011', '040008']
-    
-    # read command line arguments
+    codelist = ['160706', '165511', '040008', '110022', '110011', '020003', '160211']
+
+    # Read command line arguments
     if len(sys.argv) > 1:
         startdate = sys.argv[1]   
         if len(sys.argv) > 2:
@@ -31,27 +31,31 @@ if __name__ == "__main__":
 
     # portfolio weights
     weightlist = [1/len(codelist) for i in codelist]
-    
+
     print(startdate, enddate, codelist)
-    
+
+    # Generate URLs from start date, end date and code list
     urls = ['http://data.funds.hexun.com/outxml/detail/openfundnetvalue.aspx?fundcode='
           +fundcode+'&startdate='+startdate+'&enddate='+enddate
           for fundcode in codelist]
     namelist = []
     timeserieslist = []
-    
+
+    # Create time series from XML content at the URLs
     for url in urls:
         req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
         data = urlopen(req).read()
         tree = ET.fromstring(data)
+        
+        # Append dates and net asset values to the 2 lists respectively
         datelist = []
         valuelist = []
-        
         for child in tree:
             if child.tag == 'Data':
                 datelist.append(datetime.strptime(child.find('fld_enddate').text, '%Y-%m-%d'))
                 valuelist.append(float(child.find('fld_netvalue').text))
-        
+
+        # Create a time series from the lists
         valuelist = [i/valuelist[-1] for i in reversed(valuelist)]
         datelist = list(reversed(datelist))
         fundname = tree.find('fundcode').text + ' ' + tree.find('fundname').text
@@ -60,19 +64,25 @@ if __name__ == "__main__":
         y = np.array(valuelist)
         ts = pd.Series(y, index=x, name=fundname)
         timeserieslist.append(ts)
-    
+
+    # Concatenate time series
     df = pd.concat(timeserieslist, axis=1)
+
+    # The portfolio is a weighted sum of the funds
     df2 = df.copy()
     for i in range(len(weightlist)):
         df2[namelist[i]] = df2[namelist[i]] * weightlist[i]
-    
+
     portfolioname = 'portfolio'
-    df[portfolioname] = df2.sum(axis=1, skipna=False)
+    df[portfolioname] = df2.sum(axis=1, skipna=False)  
+
+    # Calculate risk-adjusted return
     std = df.std()
     retstd = (df.iloc[[-1]] - 1) / std
     risk_adjusted_return = df.iloc[[0,-1]].append(std.rename('standard deviation')).append(retstd.iloc[0].rename('return/standard deviation'))
     print(risk_adjusted_return)
-    
+
+    # Plot the funds and the portfolio
     plt.plot(df)
     myfont = fm.FontProperties(fname='C:/Windows/Fonts/msyh.ttc')
     namelist.append(portfolioname+' '+','.join(["{:.2f}".format(i) for i in weightlist]))
